903e23744672e7cf252f36729c9685497e38d9b9
arthur900530 <moneychien20639@gmail.com>
2022-04-18 22:09:44 +0800

    added some discription

--- src/main.py
+++ src/main.py
@@ -15,12 +15,12 @@ def get_path(base):
 
 vid_infos = vd.get_vid_paths('video.csv')
 finish_download = vd.download_vid(vid_infos)
-# finish_download = True
 
-# if finish_download:
-#     vid_paths = get_path('../inputs/full_game_1080p')
-#     # vid_path = '../inputs/full_game_1080p/CTC_A_jump.mp4'
-#     for vid_path in vid_paths:
-#         print(vid_path)
-#         success, vid_info = preprocess.video_preprocess(vid_path)
-#         print(success, vid_info)
+if finish_download:
+    print('Start resolving...')
+    vid_paths = get_path('../inputs/full_game_1080p')
+    for vid_path in vid_paths:
+        print(vid_path)
+        success, vid_info = preprocess.video_preprocess(vid_path)
+        print(success, vid_info)
+    print('Finish resolving!')
