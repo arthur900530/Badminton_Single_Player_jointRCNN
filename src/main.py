0f3009aae873377c064a3c47a21282bf7fabf3f2
arthur900530 <moneychien20639@gmail.com>
2022-04-24 22:22:32 +0800

    get_path method removed

--- src/main.py
+++ src/main.py
@@ -1,16 +1,6 @@
 import preprocess
 from scene_utils import scene_classifier
 import video_download as vd
-import os
-
-
-def get_path(base):
-    paths = []
-    with os.scandir(base) as entries:
-        for entry in entries:
-            paths.append(base + '/' + entry.name)
-            pass
-    return paths
 
 
 # vid_infos = vd.get_vid_paths('video.csv')
@@ -19,12 +9,12 @@ finish_download = True
 
 if finish_download:
     print('Start resolving...')
-    vid_paths = get_path('../inputs/full_game_1080p')
+    vid_paths, _ = preprocess.get_path('../inputs/full_game_1080p')
     total = len(vid_paths)
     current = 1
     proccessed = 0
     for vid_path in vid_paths:
-        print(f'Progress: {current}/{total}\nFailed: {proccessed}', '\n', 'Current path: ', vid_path)
+        print(f'Progress: {current}/{total}\nFailed: {proccessed}\nCurrent path:  {vid_path}')
         success, vid_info = preprocess.video_preprocess(vid_path)
         if success:
             current += 1
