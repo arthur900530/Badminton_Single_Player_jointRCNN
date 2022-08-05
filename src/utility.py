26b68e5bb9b5a1622a918715184b2bc5b96aa6c9
arthur900530 <moneychien20639@gmail.com>
2022-08-05 22:34:14 +0800

    start end time added

--- src/utility.py
+++ src/utility.py
@@ -13,3 +13,22 @@ def get_path(base):
             paths.append(base + '/' + entry.name)
             pass
     return paths
+
+
+def parse_time(FPS, frame_count):
+    start_sec = int(frame_count / FPS)
+
+    ssec = start_sec % 60
+    smin = start_sec // 60
+    if smin >= 60:
+        smin = smin % 60
+    shr = start_sec // 3600
+
+    if ssec < 10:
+        start_sec = '0' + str(start_sec)
+    if smin < 10:
+        smin = '0' + str(smin)
+    if shr < 10:
+        shr = '0' + str(shr)
+
+    return f'{shr}:{smin}:{ssec}'
